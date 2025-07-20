7dfdc01fdaad9cd0dee75c9659988d02633fe753
Mahmudul Islam Mahin <144619256+itzMahin06@users.noreply.github.com>
2025-07-20 09:25:54 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: import.meta.env.AIzaSyD1ct_CHVFa8enszqZHzqndattJ9n5iPvI,
+  authDomain: import.meta.env.live-tv-e971c.firebaseapp.com,
+  databaseURL: import.meta.env.https://live-tv-e971c-default-rtdb.firebaseio.com,
+  projectId: import.meta.env.live-tv-e971c,
+  storageBucket: import.meta.env.live-tv-e971c.firebasestorage.app,
+  messagingSenderId: import.meta.env.681248959876,
+  appId: import.meta.env.1:681248959876:web:05d72d501fc27b258741d1
+};
